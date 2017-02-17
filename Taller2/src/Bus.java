0b8b9c3e57360a52ba8f6c183b3357aecaff6da1
chprats <chprats@yahoo.com.ar>
2017-02-17 20:35:15 -0300

    Avances clase bus

--- Taller2/src/Bus.java
+++ Taller2/src/Bus.java
@@ -1,4 +1,34 @@
 
 public class Bus {
+	
+	private  String matricula;
+	private String marca;
+	private int capacidad;
+	
+
+	public String getMatricula() {
+		return matricula;
+	}
+
+	public void setMatricula(String matricula) {
+		this.matricula = matricula;
+	}
+
+	public String getMarca() {
+		return marca;
+	}
+
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+
+	public int getCapacidad() {
+		return capacidad;
+	}
+
+	public void setCapacidad(int capacidad) {
+		this.capacidad = capacidad;
+	}
+	
 
 }
