45dec04518154674c946c5c812aad01a63ba5f17
chprats <chprats@yahoo.com.ar>
2017-02-17 21:59:36 -0300

    Pruebas en clase Diccionario con TreeMap

---
+++ Taller2/src/Diccionario.java
@@ -0,0 +1,19 @@
+import java.util.TreeMap;
+
+
+
+public class Diccionario {
+	
+	private TreeMap diccio = new TreeMap();
+
+	public TreeMap getDiccio() {
+		return diccio;
+	}
+
+	public void setDiccio(TreeMap diccio) {
+		this.diccio = diccio;
+	}
+
+	
+
+}
