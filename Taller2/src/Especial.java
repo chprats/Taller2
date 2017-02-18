e61bc6615989a0fa25e5a348b3be7b663a75b925
chprats <chprats@yahoo.com.ar>
2017-02-17 21:17:48 -0300

    Se agrega la clase extendida de Boleto Especial

---
+++ Taller2/src/Especial.java
@@ -0,0 +1,15 @@
+
+public class Especial extends Boleto {
+	
+	private float descuento;
+
+	public float getDescuento() {
+		return descuento;
+	}
+
+	public void setDescuento(float descuento) {
+		this.descuento = descuento;
+	}
+	
+
+}
