6381066f6a28d09b2b5584775ddf136aea71ba7d
German <German@German-PC>
2017-02-17 22:32:41 -0200

    Saque atributo

--- Taller2/src/Boleto.java
+++ Taller2/src/Boleto.java
@@ -6,7 +6,6 @@ public class Boleto {
 	private String lugar;
 	private String celular;
 	private String STM;
-	private String COECT;
 	
 public Boleto(int nroBoleto, int edad, String lugar, String celular){
 		
