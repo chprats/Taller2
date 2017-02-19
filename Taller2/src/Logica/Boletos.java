ead102dc23836202493ccdc93f91202e90e3a0cb
chprats <chprats@yahoo.com.ar>
2017-02-18 21:17:34 -0300

    Se crea la clase colección Boletos

---
+++ Taller2/src/Logica/Boletos.java
@@ -0,0 +1,21 @@
+package Logica;
+import java.util.*;
+
+import Logica.boleto.Boleto;
+
+
+public class Boletos {
+	
+	private ArrayList<Boleto>  arreLiBoletos = new ArrayList<Boleto>();
+
+	public ArrayList<Boleto> getArreLiBus() {
+		return arreLiBoletos;
+	}
+
+	public void setArreLiBus(ArrayList<Boleto> arreLiBoletos) {
+		this.arreLiBoletos = arreLiBoletos;
+	}
+
+	
+
+}
